848a0090699006e33ff262edaecb0817cd330c80
kunj1864 <kunjdarji12@gmail.com>
2026-01-16 20:26:29 +0530

    Final List and Settings Update

--- intelligent-tutor-system/src/api.jsx
+++ intelligent-tutor-system/src/api.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 // const API_URL = 'http://127.0.0.1:8000/api/';
 
 
-const API_URL = 'https://ai-tutor-system-1.onrender.com/api/'; 
+const API_URL = 'https://ai-tutor-system-1.onrender.com/'; 
 
 const api = axios.create({
   baseURL: API_URL,
